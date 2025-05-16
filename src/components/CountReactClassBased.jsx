21efddd59bdcdbca41d1583277bc673861f41d7d
NikhilAndola <nikhil@coredge.io>
2025-05-16 16:27:46 +0530

    Commit for ErrorBoundary

--- src/components/CountReactClassBased.jsx
+++ src/components/CountReactClassBased.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ErrorBoundary from './ErrorBoundary'
 
 // Challenge:
 // Given an incomplete class-based component without a constructor,
@@ -34,6 +35,11 @@ class CountReactClassBased extends Component {
         <h3>{this.state.age} years old</h3>
         <h4>{this.state.count}</h4>
         <button onClick={this.handleCount}>Click to Update</button>
+
+        <button onClick={()=> {}}>Set Error</button>
+        <ErrorBoundary fallback={<h3>Error has occured</h3>}>
+          <div>List of items</div>
+        </ErrorBoundary>
       </div>
     );
   }
